2b20bfe1814c5730d27955013f057e9f521871bf
kstrickland0612 <kstrickland0612@gmail.com>
2019-05-23 19:05:22 -0400

    Fix item count bug
    - add orders/delete/:id patch request

--- assets/scripts/actions/api.js
+++ assets/scripts/actions/api.js
@@ -45,7 +45,7 @@ const addToCart = function () {
 
 const deleteFromCart = function () {
   return $.ajax({
-    url: config.apiUrl + `/orders/${store.currentOrder.order._id}`,
+    url: config.apiUrl + `/orders/delete/${store.currentOrder.order._id}`,
     method: 'PATCH',
     headers: {
       authorization: 'Token token=' + store.user.token
